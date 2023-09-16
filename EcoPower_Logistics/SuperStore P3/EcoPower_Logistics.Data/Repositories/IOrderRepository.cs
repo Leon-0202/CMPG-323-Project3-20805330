2d16872a4b88b6fe78cad52c4e53a1735858a633
Leon <ernestmostert0202@gmail.com>
2023-09-16 20:34:06 +0200

    Added repository interface and class for Order

---
+++ EcoPower_Logistics/SuperStore P3/EcoPower_Logistics.Data/Repositories/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EcoPower_Logistics.Data.Models;
+
+namespace EcoPower_Logistics.Data.Repositories
+{
+    public interface IOrderRepository : IGenericRepository<Order>
+    {
+        Order GetOrderById(int? id);
+        IEnumerable<Order> GetAllOrders();
+        void AddOrder(Order entity);
+        void UpdateOrder(Order entity);
+        void RemoveOrder(Order entity);
+    }
+}
