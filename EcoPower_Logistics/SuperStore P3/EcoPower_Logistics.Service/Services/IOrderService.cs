617a5d2e8e4215582d7da7ef8bea62b79f9bf646
Leon <ernestmostert0202@gmail.com>
2023-09-16 22:10:33 +0200

    Added Service classes for Order.

---
+++ EcoPower_Logistics/SuperStore P3/EcoPower_Logistics.Service/Services/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EcoPower_Logistics.Data.Models;
+
+namespace EcoPower_Logistics.Service.Services
+{
+    public interface IOrderService
+    {
+        Order GetOrderById(int? id);
+        IEnumerable<Order> GetAllOrders();
+        void AddOrder(Order entity);
+        void UpdateOrder(Order entity);
+        void RemoveOrder(Order entity);
+    }
+}
---
+++ EcoPower_Logistics/SuperStore P3/EcoPower_Logistics.Service/Services/OrderService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EcoPower_Logistics.Data.Models;
+using EcoPower_Logistics.Data.Repositories;
+
+namespace EcoPower_Logistics.Service.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public Order GetOrderById(int? id)
+        {
+            return _orderRepository.GetAll().FirstOrDefault(x => x.OrderId == id);
+        }
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return _orderRepository.GetAll().ToList();
+        }
+
+        public void AddOrder(Order entity)
+        {
+            _orderRepository.Add(entity);
+        }
+
+        public void UpdateOrder(Order entity)
+        {
+            _orderRepository.Update(entity);
+        }
+
+        public void RemoveOrder(Order entity)
+        {
+            _orderRepository.Remove(entity);
+        }
+    }
+}
